098fb154e59288877e6a2509b3279ad9c7189aec
kwakseongjae <gkffhdnls13@gmail.com>
2024-08-20 10:59:38 +0900

    chore: tailwind config color extend 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,16 @@ const config: Config = {
       fontFamily: {
         pretendard: ['var(--font-pretendard)'],
       },
+      colors: {
+        primary: '#0046FF',
+        '_blue-100': '#8CD2F5',
+        '_blue-200': '#4BAFF5',
+        '_blue-300': '#2878F5',
+        '_blue-400': '#00236E',
+        '_grey-100': '#EEEEEE',
+        '_grey-200': '#D2D2D2',
+        '_grey-300': '#929292',
+      },
     },
   },
   plugins: [],
