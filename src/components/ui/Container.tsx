793f2ad5ed4802891cfb2db383db8ecd36689763
kwakseongjae <gkffhdnls13@gmail.com>
2024-08-22 18:27:51 +0900

    fix: 불필요한 스크롤바 생성 수정

--- src/components/ui/Container.tsx
+++ src/components/ui/Container.tsx
@@ -13,7 +13,7 @@ export default function Container({
 
   return (
     <div
-      className={`relative w-full max-h-dvh min-h-dvh overflow-y-scroll ${navState ? 'pt-[3.75rem]' : ''} ${menuState ? 'pb-[4.5rem]' : ''} px-5`}
+      className={`relative w-full min-h-dvh max-h-dvh ${navState ? 'pt-[3.75rem]' : ''} ${menuState ? 'pb-[4.5rem]' : ''} px-5`}
     >
       {children}
     </div>
