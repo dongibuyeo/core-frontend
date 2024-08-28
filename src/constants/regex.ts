de9b29552c3e212cca136526ac2bf19c103a88d2
kwakseongjae <gkffhdnls13@gmail.com>
2024-08-28 18:05:07 +0900

    chore: 이름 정규표현식 추가

--- src/constants/regex.ts
+++ src/constants/regex.ts
@@ -2,6 +2,7 @@
 
 // 회원가입
 export const EMAIL_REGEX = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+export const USERNAME_REGEX = /^[가-힣]{1,8}$/
 export const NICKNAME_REGEX = /^[a-zA-Z0-9가-힣]{1,10}$/
 
 // URL
