4b14dba1581ea74bb9f2bbdddf18020653b544c9
kwakseongjae <gkffhdnls13@gmail.com>
2024-08-28 09:57:43 +0900

    design: padding을 부모 요소로 이동시켜 UI를 범용성있게 변경

--- src/containters/challenge/[id]/ChallengeRanking.tsx
+++ src/containters/challenge/[id]/ChallengeRanking.tsx
@@ -2,10 +2,10 @@ import ChallengeRankingCard from '@/containters/challenge/ChallengeRankingCard'
 
 export default function ChallengeRanking() {
   return (
-    <div className="w-full px-5 pb-12">
+    <div className="w-full pb-12">
       <div className="flex flex-col gap-7">
         <div className="bg-_grey-100 w-full p-4 mt-4 text-center rounded-xl font-medium">
-          <p className="text-base">현재 상위 10% 커트라인 점수</p>
+          <p className="text-base">상위 10% 커트라인 점수</p>
           <p className="mt-1 text-xl">
             <span className="font-semibold text-primary">182</span>점
           </p>
