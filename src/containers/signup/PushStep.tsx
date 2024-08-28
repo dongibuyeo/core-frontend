39e3281b24687973a6a5971c1432357dc87b91cb
kwakseongjae <gkffhdnls13@gmail.com>
2024-08-28 18:06:34 +0900

    feat: 회원가입 api 연결

--- src/containers/signup/PushStep.tsx
+++ src/containers/signup/PushStep.tsx
@@ -1,13 +1,35 @@
+/* eslint-disable no-console */
 import Button from '@/components/ui/Button'
 import { getTokenHandler } from '@/firebase/firebase'
 import { Info } from '@/public/svg/index'
 import Image from 'next/image'
-// import useSignupStore from '@/store/SignupStore'
+import useSignupStore from '@/store/SignupStore'
 
 import { useRouter } from 'next/navigation'
+import { useMutation } from '@tanstack/react-query'
+import { useRef } from 'react'
+import toast from 'react-hot-toast'
+import { signupUser } from '@/services/auth'
 
 export default function PushStep() {
   const router = useRouter()
+  const { username, email, nickname } = useSignupStore((state) => state)
+  const token = useRef('')
+
+  const mutation = useMutation({
+    mutationFn: async () =>
+      signupUser(email, username, nickname, { current: token.current }),
+
+    onSuccess: () => {
+      localStorage.setItem('email', email)
+      localStorage.removeItem('signupState')
+      toast.success('회원가입 되었습니다.')
+      router.push('/home')
+    },
+    onError: (error) => {
+      console.error('회원가입 중 에러가 발생했습니다.', error)
+    },
+  })
 
   const clickPushHandler = async () => {
     try {
@@ -17,23 +39,23 @@ export default function PushStep() {
         })
         .then(async (reg) => {
           console.log('Service worker registered:', reg)
-          const token = await getTokenHandler()
-          if (token) {
-            console.log('푸시 토큰:', token)
-            router.push('/signup/join')
+          const pushToken = await getTokenHandler()
+          if (pushToken) {
+            token.current = pushToken
+            mutation.mutate()
           } else {
             console.error('푸시 토큰이 없습니다.')
           }
         })
     } catch (error) {
-      console.error('푸시 토큰 가져오는 중에 에러 발생', error)
+      console.error('푸시 토큰 가져오는 중에 에러가 발생했습니다.')
     }
   }
 
   return (
     <div className="flex flex-col w-full h-dvh justify-start p-5">
       <div className="pt-[6.25rem] mx-auto">
-        <Image src="/gif/bell.gif" width={90} height={90} alt="알림 설정 벨" />
+        <Image src="/gif/bell.gif" width={90} height={90} alt="알림 설정" />
       </div>
       <div className="flex flex-col gap-12 mt-12">
         <h1 className="text-center text-xl font-semibold">푸시 알림 받기</h1>
@@ -69,7 +91,7 @@ export default function PushStep() {
         <Button
           text="다음에 할게요!"
           className="w-full bg-transparent text-[#626262]"
-          onClick={() => router.push('/signup/join')}
+          onClick={mutation.mutate}
         />
       </div>
     </div>
