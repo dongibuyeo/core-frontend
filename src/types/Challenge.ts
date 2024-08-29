141beac5a4b3950d2f304b3f435adcc900ed27a1
kwakseongjae <gkffhdnls13@gmail.com>
2024-08-29 11:08:20 +0900

    chore: Challenge 타입 추가

---
+++ src/types/Challenge.ts
@@ -0,0 +1,13 @@
+export type Challenge = {
+  challengeId: string
+  type?: string
+  status?: string
+  accountNo?: string
+  startDate: string
+  endDate: string
+  title: string
+  description?: string
+  totalDeposit: string
+  participants: string
+  image: string
+}
