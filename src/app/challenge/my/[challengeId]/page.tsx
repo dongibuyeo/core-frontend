c406d602ae7ce70ab1140e91ac86449b6ef7b445
kwakseongjae <gkffhdnls13@gmail.com>
2024-08-30 01:21:20 +0900

    feat: 내챌린지 메인페이지 API 연결 및 라우팅 작업

---
+++ src/app/challenge/my/[challengeId]/page.tsx
@@ -0,0 +1,7 @@
+export default function MyChallengeDetail() {
+  return (
+    <div>
+      <h1>챌린지 상세보기 페이지입니다.</h1>
+    </div>
+  )
+}
---
+++ src/app/challenge/my/page.tsx
@@ -0,0 +1,84 @@
+/* eslint-disable no-nested-ternary */
+/* eslint-disable @typescript-eslint/no-unused-expressions */
+
+'use client'
+
+import ChallengeStatusButton from '@/components/ChallengeStatusButton'
+import MyChallengeCard from '@/components/MyChallengeCard'
+import { getUserInfo } from '@/services/auth'
+import { getMyChallengeList } from '@/services/challenges'
+import { ChallengeStatus } from '@/types/ChallengeStatus'
+import { useQuery } from '@tanstack/react-query'
+import { useEffect, useState } from 'react'
+import Image from 'next/image'
+import { MemberChallengeDetail, MyChallengeList } from '@/types/MyChallenge'
+
+export default function MyChallengePage() {
+  const [challengeStatus, setChallengeStatus] =
+    useState<ChallengeStatus>('SCHEDULED')
+  const [filteredChallengeList, setFilteredChallengeList] =
+    useState<MemberChallengeDetail[]>()
+
+  const { data: user } = useQuery({
+    queryKey: ['user'],
+    queryFn: () => getUserInfo(),
+  })
+
+  const { data: myChallengeList, isPending } = useQuery<MyChallengeList>({
+    queryKey: ['myChallengeList'],
+    queryFn: () => getMyChallengeList(user?.memberId as string),
+    enabled: !!user?.memberId,
+  })
+
+  useEffect(() => {
+    if (!myChallengeList) return
+
+    const filteredList: MemberChallengeDetail[] =
+      myChallengeList?.memberChallengeDetails?.filter(
+        (challenge) => challenge?.status === challengeStatus,
+      )
+    setFilteredChallengeList(filteredList)
+  }, [challengeStatus, myChallengeList])
+
+  return (
+    <div className="pt-6 px-5">
+      <ChallengeStatusButton
+        challengeStatus={challengeStatus}
+        setChallengeStatus={setChallengeStatus}
+        tabType="my"
+      />
+      <div className="mt-7 flex flex-col gap-4">
+        {filteredChallengeList?.map((challenge) => (
+          <MyChallengeCard
+            title={challenge?.title}
+            startDate={challenge?.startDate}
+            endDate={challenge?.endDate}
+            imageUrl={challenge?.image}
+            status={challenge?.status}
+            memberStatus={challenge?.memberStatus}
+            challengeId={challenge?.challengeId}
+            type={challenge?.type}
+          />
+        ))}
+        {!isPending && filteredChallengeList?.length === 0 && (
+          <div className="flex flex-col gap-7 justify-center items-center h-96">
+            <Image
+              src="/gif/crying-face.gif"
+              alt="not-found"
+              width={80}
+              height={80}
+            />
+            <p className="text-center font-medium text-_grey-400">
+              {challengeStatus === 'SCHEDULED'
+                ? '참여예정인'
+                : challengeStatus === 'IN_PROGRESS'
+                  ? '참여중인'
+                  : '완료한'}{' '}
+              챌린지가 없습니다!
+            </p>
+          </div>
+        )}
+      </div>
+    </div>
+  )
+}
